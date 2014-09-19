6fbab5b17fffc177afdf6cb5f2aeb27a393549be
David Stancu <dstancu@nyu.edu>
2014-09-18 20:50:27 -0400

    bump version

--- lib/client.php
+++ lib/client.php
@@ -15,7 +15,7 @@
  * @author Dwolla (David Stancu): api@dwolla.com, david@dwolla.com
  * @copyright Copyright (C) 2014 Dwolla Inc.
  * @license  MIT (http://opensource.org/licenses/MIT)
- * @version 2.0.0
+ * @version 2.0.1
  * @link http://developers.dwolla.com
  */
 
