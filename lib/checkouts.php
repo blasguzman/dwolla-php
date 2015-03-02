cc5d75d20571532110fbcd2bdf8c6f93026cacbd
David Stancu <dstancu@nyu.edu>
2015-03-02 12:13:24 -0500

    two more unescaped fixes

--- lib/checkouts.php
+++ lib/checkouts.php
@@ -78,8 +78,8 @@ class Checkouts extends RestClient {
     public function create($purchaseOrder, $params = false) {
         if (!$purchaseOrder) { return self::_error("create() requires `\$purchaseOrder` parameter.\n"); }
         if (is_array($purchaseOrder)) {
-            if (!$purchaseOrder['destinationId']) { return self::_error("`$purchaseOrder` has no `destinationId` key."); }
-            if (!$this->cart && !$purchaseOrder['total']) { return self::_error("`$purchaseOrder` has no `total` amount. Create a cart or pass in a total order amount."); }
+            if (!$purchaseOrder['destinationId']) { return self::_error("`\$purchaseOrder` has no `destinationId` key."); }
+            if (!$this->cart && !$purchaseOrder['total']) { return self::_error("`\$purchaseOrder` has no `total` amount. Create a cart or pass in a total order amount."); }
         }
         else { return self::_error("createCheckout() requires `\$purchaseOrder` to be of type array."); }
 
